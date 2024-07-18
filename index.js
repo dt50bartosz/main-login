38b1612d3a3949fb7d129296c8b7ee67f3addc50
Bartosz Jankowski <bartosz.jankowski.programista@gmail.com>
2024-07-18 17:58:05 +0200

    linked added

---
+++ index.js
@@ -0,0 +1,24 @@
+$(".add-income").click(function(event) {
+   
+    event.preventDefault(); 
+  
+    window.location.href = "https://dt50bartosz.github.io/addIncome/";
+  
+});
+
+$(".add-expense").click(function(event) {
+   
+    event.preventDefault(); 
+  
+    window.location.href = "https://github.com/dt50bartosz/addExpense/settings/pages";
+  
+});
+
+$(".add-income").click(function(event) {
+   
+    event.preventDefault(); 
+  
+    window.location.href = "https://dt50bartosz.github.io/balance/";
+  
+});
+
